bc9b533847000459cdc22f0c936d60dcb89dab4b
Abdulhakim Zeinu <zluckyza@gmail.com>
2024-06-19 15:56:56 +0300

    Basic file change detection and //report is completed

--- FileChangeMonitor/Form1.Designer.cs
+++ FileChangeMonitor/Form1.Designer.cs
@@ -6,7 +6,10 @@
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.ListBox lstChanges;
         /// <summary>
         /// Clean up any resources being used.
         /// </summary>
@@ -28,10 +31,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lstChanges = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(17, 16);
+            this.lblInstructions.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(163, 16);
+            this.lblInstructions.TabIndex = 0;
+            this.lblInstructions.Text = "Select a text file to monitor:";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(21, 37);
+            this.txtFilePath.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(763, 22);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(792, 31);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(100, 28);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lstChanges
+            // 
+            this.lstChanges.FormattingEnabled = true;
+            this.lstChanges.ItemHeight = 16;
+            this.lstChanges.Location = new System.Drawing.Point(21, 70);
+            this.lstChanges.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstChanges.Name = "lstChanges";
+            this.lstChanges.Size = new System.Drawing.Size(871, 484);
+            this.lstChanges.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(905, 584);
+            this.Controls.Add(this.lstChanges);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.lblInstructions);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "File Change Monitor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
--- FileChangeMonitor/Form1.cs
+++ FileChangeMonitor/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,75 @@ namespace FileChangeMonitor
 {
     public partial class Form1 : Form
     {
+        private string targetFilePath;
+        private FileSystemWatcher fileWatcher;
+        private Timer batchTimer;
+        private string previousContent;
+
         public Form1()
         {
             InitializeComponent();
+            InitializeFileWatcher();
+            InitializeBatchTimer();
+        }
+        private void InitializeFileWatcher()
+        {
+            fileWatcher = new FileSystemWatcher();
+            fileWatcher.NotifyFilter = NotifyFilters.LastWrite;
+            fileWatcher.Changed += OnFileChanged;
+        }
+        private void InitializeBatchTimer()
+        {
+            batchTimer = new Timer();
+            batchTimer.Interval = 15000; // 15 seconds
+            batchTimer.Tick += OnBatchTimerTick;
+            batchTimer.Start();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Text files (*.txt)|*.txt";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    targetFilePath = openFileDialog.FileName;
+                    txtFilePath.Text = targetFilePath;
+                    fileWatcher.Path = Path.GetDirectoryName(targetFilePath);
+                    fileWatcher.Filter = Path.GetFileName(targetFilePath);
+                    fileWatcher.EnableRaisingEvents = true;
+                    previousContent = File.ReadAllText(targetFilePath);
+                }
+            }
+        }
+
+        private void OnFileChanged(object sender, FileSystemEventArgs e)
+        {
+            
+        }
+
+        private void OnBatchTimerTick(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(targetFilePath))
+                return;
+
+            string currentContent = File.ReadAllText(targetFilePath);
+            if (currentContent != previousContent)
+            {
+                string changes = GetChanges(previousContent, currentContent);
+                ReportChanges(changes);
+                previousContent = currentContent;
+            }
+        }
+
+        private string GetChanges(string oldContent, string newContent)
+        {
+            return newContent;
+        }
+
+        private void ReportChanges(string changes)
+        {
+            lstChanges.Items.Add(changes);
         }
     }
 }
